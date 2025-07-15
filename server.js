3ae33eed641c662b191d97ba7b13d2a8995622a2
swatisaulanki <swatisaulanki@gmail.com>
2025-07-15 16:49:51 +0530

    updated server

--- server.js
+++ server.js
@@ -1,38 +1,3 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-
-// // Load environment variables
-// dotenv.config();
-
-// // Connect to MongoDB
-// connectDB();
-
-// // Initialize app
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/products', require('./routes/productRoutes'));
-// app.use('/api/auth', require('./routes/authRoutes'));
-// app.use('/api/cart', require('./routes/cartRoutes'));
-// app.use('/api/contact-us', require('./routes/contactUsRoutes'));
-
-// // Fallback route
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Route not found' });
-// });
-
-// // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server is running on port ${PORT}`);
-// });
-
 
 const express = require('express');
 const cors = require('cors');
@@ -47,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("🎁 Welcome to the Gifterra Backend API")
+    res.send('🎉 Welcome to the Gifterra Backend API')
 })
 
 app.use('/uploads', express.static('uploads'));
